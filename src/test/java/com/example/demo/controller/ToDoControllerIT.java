5903c557cebd2b1c27d00fe1331dae63d6743861
Zhenya Musievsky <zhenya.musievsky@gmail.com>
2020-07-14 03:46:45 +0300

    Integration tests for ToDoController added

--- src/test/java/com/example/demo/controller/ToDoControllerIT.java
+++ src/test/java/com/example/demo/controller/ToDoControllerIT.java
@@ -2,6 +2,9 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.ToDoResponse;
 import com.example.demo.dto.ToDoSaveRequest;
+import com.example.demo.dto.mapper.ToDoEntityToResponseMapper;
+import com.example.demo.exception.ToDoNotFoundException;
+import com.example.demo.model.ToDoEntity;
 import com.example.demo.service.ToDoService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -12,12 +15,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.nio.charset.Charset;
+import java.time.Clock;
+import java.time.ZonedDateTime;
 
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ToDoController.class)
@@ -30,16 +33,16 @@ public class ToDoControllerIT {
     private ToDoService toDoService;
 
     @Test
-    public void whenSave_thenReturnValidResponse() throws Exception {
+    public void whenSaveWithSaveRequest_thenReturnValidResponse() throws Exception {
         String text = "Request text";
         var request = new ToDoSaveRequest();
         request.text = text;
 
         ObjectMapper mapper = new ObjectMapper();
-        var requestJson = mapper.writeValueAsString(request);
+        var jsonRequest = mapper.writeValueAsString(request);
 
         var response = new ToDoResponse();
-        response.id = 0L;
+        response.id = 4553L;
         response.text = text;
 
         when(toDoService.upsert(ArgumentMatchers.any(ToDoSaveRequest.class)))
@@ -49,12 +52,78 @@ public class ToDoControllerIT {
                 .perform(post("/todos")
                         .contentType(MediaType.APPLICATION_JSON)
                         .characterEncoding("utf-8")
-                        .content(requestJson))
+                        .content(jsonRequest))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$").isNotEmpty())
-                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.id").value(response.id))
                 .andExpect(jsonPath("$.text").value(text))
-                .andExpect(jsonPath("$.completeAt").doesNotExist());
+                .andExpect(jsonPath("$.completedAt").doesNotExist());
+    }
+
+    @Test
+    public void whenSaveWithId_thenReturnValidResponse() throws Exception {
+        long id = 5325L;
+        String text = "Request text";
+        ZonedDateTime time = ZonedDateTime.now(Clock.systemUTC());
+        var toDoEntity = new ToDoEntity(id, text, time);
+
+        when(toDoService.completeToDo(id)).then(i -> ToDoEntityToResponseMapper.map(toDoEntity));
+
+        this.mockMvc
+                .perform(put(String.format("/todos/%s/complete", id))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$.id").value(id))
+                .andExpect(jsonPath("$.text").value(text))
+                .andExpect(jsonPath("$.completedAt").exists());
+    }
+
+    @Test
+    public void whenSaveWithIdNotFound_throwException() throws Exception {
+        long id = 412412L;
+        String text = "Request text";
+        ObjectMapper mapper = new ObjectMapper();
+        var jsonRequest = mapper.writeValueAsString(id);
+
+        when(toDoService.completeToDo(id)).thenThrow(ToDoNotFoundException.class);
+
+        this.mockMvc
+                .perform(put(String.format("/todos/%s/complete", id))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8")
+                        .content(jsonRequest))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$").doesNotExist());
+    }
+
+    @Test
+    public void whenGetOne_thenReturnValidResponse() throws Exception {
+        long id = 4124L;
+        String text = "Request text";
+        var toDoEntity = new ToDoEntity(id, text);
+
+        when(toDoService.getOne(id)).then(a -> ToDoEntityToResponseMapper.map(toDoEntity));
+
+        this.mockMvc.perform(get(String.format("/todos/%s", id)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$.id").value(id))
+                .andExpect(jsonPath("$.text").value(text));
+    }
+
+    public void whenGetOneWithIdNotFound() throws Exception {
+        long id = 4124L;
+
+        when(toDoService.getOne(id)).thenThrow(ToDoNotFoundException.class);
+
+        this.mockMvc.perform(get(String.format("/todos/%s", id)))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$").doesNotExist());
+
     }
 }
